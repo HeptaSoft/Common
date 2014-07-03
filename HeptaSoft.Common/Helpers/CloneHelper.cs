f7c6ee482daaa1bfa658973079125110915e576f
organom <ricardogomes@royalhat.org>
2014-07-03 16:32:03 +0200

    Initial submit of the clone helper + tests. Still needs some work.

--- HeptaSoft.Common/Helpers/CloneHelper.cs
+++ HeptaSoft.Common/Helpers/CloneHelper.cs
@@ -1,11 +1,105 @@
-﻿
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Reflection;
+
 namespace HeptaSoft.Common.Helpers
 {
     /// <summary>
-    ///  Clone Helper class
+    /// Clone Helper.
     /// </summary>
     public static class CloneHelper
     {
+        /// <summary> 
+        /// Determines whether the type represents a direct value. 
+        /// </summary> 
+        /// <param name="valueType">Type of the value.</param> 
+        /// <returns><c>True</c> is the specified type is a direct value.</returns> 
+        private static bool IsDirectValue(Type valueType)
+        {
+            return (valueType == typeof(string))
+                || (valueType == typeof(DateTime))
+                || (valueType == typeof(DateTime?))
+                || (valueType == typeof(Guid))
+                || (valueType == typeof(Guid?))
+                || (valueType == typeof(Decimal))
+                || (valueType == typeof(Decimal?))
+                || (valueType.IsPrimitive)
+                || (valueType.IsEnum);
+        }
 
+        /// <summary>
+        /// Clones a list.
+        /// </summary>
+        /// <param name="originalList">The original list.</param>
+        /// <param name="destinationList">The destination list.</param>
+        /// <returns>
+        /// A cloned list.
+        /// </returns>
+        private static IList DoCloneList(IList originalList, IList destinationList)
+        {
+            foreach (var originalElement in originalList)
+            {
+                destinationList.Add(DoClone(originalElement));
+            }
+            return destinationList;
+        }
+
+        /// <summary>
+        /// Creates a clone.
+        /// </summary>
+        /// <param name="originalInstance">The original instance.</param>
+        /// <returns>The clone.</returns>
+        private static object DoClone(object originalInstance)
+        {
+            // new instance of the runtime "real" type of the originalInstance object.
+            var clone = originalInstance.GetType().GetConstructors().First().Invoke(null);
+            // gets all the fields on the instance
+            foreach (var field in originalInstance.GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+            {
+                var originalValue = field.GetValue(originalInstance);
+                if (originalValue != null)
+                {
+                    Type originalValueType = originalValue.GetType();
+
+                    if (IsDirectValue(originalValueType))
+                    {
+                        // Is a direct value
+                        field.SetValue(clone, originalValue);
+                    }
+                    else if (originalValueType.IsArray)
+                    {
+                        // Is an array
+                        var oldArray = (Array)originalValue;
+                        var newArray = (Array)originalValueType.GetConstructors().First().Invoke(new object[] { oldArray.Length });
+                        Array.Copy(oldArray, newArray, oldArray.Length);
+                        field.SetValue(clone, newArray);
+                    }
+                    else if (typeof(IList).IsAssignableFrom(originalValueType))
+                    {
+                        // It descends from IList
+                        var list = originalValueType.GetConstructors().First().Invoke(null);
+                        field.SetValue(clone, DoCloneList(originalValue as IList, list as IList));
+                    }
+                    else
+                    {
+                        // Is Other object
+                        field.SetValue(clone, DoClone(originalValue));
+                    }
+                }
+            }
+            return clone;
+        }
+
+        /// <summary>
+        /// Clones the specified original instance.
+        /// </summary>
+        /// <typeparam name="T">Type of the instance.</typeparam>
+        /// <param name="originalInstance">The original instance.</param>
+        /// <returns>The cloned instance.</returns>
+        public static T Clone<T>(T originalInstance)
+        {
+            return (T)DoClone(originalInstance);
+        }
     }
 }
---
+++ HeptaSoft.CommonTests/Helpers/CloneHelperTests.cs
@@ -0,0 +1,117 @@
+﻿using HeptaSoft.Common.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace HeptaSoft.CommonTests.Helpers
+{
+    public class SingleLevelPrimitives
+    {
+        public Guid Id { get; set; }
+        public bool Boolean = true;
+        public string StringVal = "One name";
+        public string StringProp { get; set; }
+        public double NumVal = 10.9;
+        public decimal NumVal2 = 11;
+        public long NumProp { get; set; }
+        public float NumProp2 { get; set; }
+        public byte ByteVal = 3;
+        public byte ByteProp { get; set; }
+        public object ObjectVal = (object)"Hello";
+        public object ObjectProp { get; set; }
+        public char CharVal = 'a';
+        public char CharProp { get; set; }
+
+        public SingleLevelPrimitives()
+        {
+            this.Id = Guid.NewGuid();
+            this.StringProp = "Hello class";
+            this.NumProp = 1111111;
+            this.NumProp2 = (float)0.1;
+            this.ByteProp = (byte)1;
+            this.ObjectProp = (object)"World!";
+            this.CharProp = 'b';
+        }
+    }
+
+    public class MultiLevelClass
+    {
+        public Guid MLId { get; set; }
+        public bool MLBoolean = true;
+        public SingleLevelPrimitives primitivesClass = new SingleLevelPrimitives();
+        public SingleLevelPrimitives primitivesClassProp { get; set; }
+
+        public MultiLevelClass()
+        {
+            this.MLId = Guid.NewGuid();
+            this.primitivesClassProp = new SingleLevelPrimitives();
+        }
+    }
+
+    [TestClass]
+    public class CloneHelperTests
+    {
+        private void AssertEqualPrimitiveClass(SingleLevelPrimitives primitiveClass1, SingleLevelPrimitives primitiveClass2)
+        {
+            #region Helpers
+
+            Assert.AreNotEqual(primitiveClass1, primitiveClass2);
+
+            Assert.AreEqual(primitiveClass1.Id, primitiveClass2.Id);
+            Assert.AreEqual(primitiveClass1.Boolean, primitiveClass2.Boolean);
+            Assert.AreEqual(primitiveClass1.StringVal, primitiveClass2.StringVal);
+            Assert.AreEqual(primitiveClass1.StringProp, primitiveClass2.StringProp);
+            Assert.AreEqual(primitiveClass1.NumVal, primitiveClass2.NumVal);
+            Assert.AreEqual(primitiveClass1.NumVal2, primitiveClass2.NumVal2);
+            Assert.AreEqual(primitiveClass1.NumProp, primitiveClass2.NumProp);
+            Assert.AreEqual(primitiveClass1.NumProp2, primitiveClass2.NumProp2);
+            Assert.AreEqual(primitiveClass1.ByteVal, primitiveClass2.ByteVal);
+            Assert.AreEqual(primitiveClass1.ByteProp, primitiveClass2.ByteProp);
+            Assert.AreEqual(primitiveClass1.ObjectVal, primitiveClass2.ObjectVal);
+            Assert.AreEqual(primitiveClass1.ObjectProp, primitiveClass2.ObjectProp);
+            Assert.AreEqual(primitiveClass1.CharVal, primitiveClass2.CharVal);
+            Assert.AreEqual(primitiveClass1.CharProp, primitiveClass2.CharProp);
+        }
+
+            #endregion
+
+        [TestMethod]
+        public void CloneClass1_SingleLevelPrimitives_AllEqual()
+        {
+            var testClass = new SingleLevelPrimitives();
+
+            var clonedTestClass = CloneHelper.Clone(testClass);
+
+            Assert.IsNotNull(clonedTestClass);
+            AssertEqualPrimitiveClass(testClass, clonedTestClass);
+        }
+
+        [TestMethod]
+        public void CloneClass1_MultiLevelClass_AllEqual()
+        {
+            var testClass = new MultiLevelClass();
+
+            var clonedTestClass = CloneHelper.Clone(testClass);
+
+            Assert.IsNotNull(clonedTestClass);
+            Assert.AreEqual(testClass.MLId, clonedTestClass.MLId);
+            Assert.AreEqual(testClass.MLBoolean, clonedTestClass.MLBoolean);
+            AssertEqualPrimitiveClass(testClass.primitivesClass, clonedTestClass.primitivesClass);
+            AssertEqualPrimitiveClass(testClass.primitivesClassProp, clonedTestClass.primitivesClassProp);
+        }
+
+        [TestMethod]
+        public void CloneClass1_MultiLevelClass_AbleToHandleNull()
+        {
+            var testClass = new MultiLevelClass();
+            testClass.primitivesClass = null;
+
+            var clonedTestClass = CloneHelper.Clone(testClass);
+
+            Assert.IsNotNull(clonedTestClass);
+            Assert.AreEqual(testClass.MLId, clonedTestClass.MLId);
+            Assert.AreEqual(testClass.MLBoolean, clonedTestClass.MLBoolean);
+            Assert.IsNull(clonedTestClass.primitivesClass);
+            AssertEqualPrimitiveClass(testClass.primitivesClassProp, clonedTestClass.primitivesClassProp);
+        }
+    }
+}
